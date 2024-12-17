af605099463adc43ce227f66a695245dc2ae1b1e
SATHISHKUMAR.M <sathish323334@gmail.com>
2024-12-17 10:44:13 +0530

    sathish

---
+++ string_task_2.js
@@ -0,0 +1,61 @@
+//Task 1: Concatenate Two Strings
+
+let string_1 = "HELLO";
+let string_2 = " WORLD";
+console.log(string_1.concat(string_2));
+
+//Task 2: Find the Length of a String
+
+let string_3 = "PYTHON";
+console.log(string_3.length);
+
+
+//Task 3: Extract a Substring
+
+let string_5 = "HELLO!WORLD";
+console.log(string_5.slice(6,11));
+
+
+
+//Task 4: Convert a String to Uppercase
+
+let string_6 = "sathishkumar";
+console.log(string_6.toUpperCase());
+
+//Task 5: Check if a String Contains a Substring
+
+let string_7 = "I LOVE PYTHON";
+
+console.log(string_7.endsWith("PYTHON"))
+
+//Task 6: Replace a Substring
+
+
+let string_8 = "HELLO WORLD";
+console.log(string_8.replace("WORLD","JAVASCRIPT"));
+
+
+
+//Task 7: Trim Whitespace
+
+let string_9 = " HELLO WORLD! ";
+console.log(string_9);
+console.log(string_9.trim());
+
+
+//Task 8: Split a String into an Array
+
+let string_10 = "apple,banana,cherry";
+console.log(string_10.split(","));
+
+
+//Task 9: Repeat a String
+
+
+let string_11 = "HA!";
+console.log(string_11.repeat(3));
+
+
+//Task 10: Check the First Character of a String
+let string_12 = "PYTHON";
+console.log(string_12.startsWith("P"))
